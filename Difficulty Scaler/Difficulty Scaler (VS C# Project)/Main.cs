336438895095addf5065656f5c6f4a860723f076
Kasper Topp <toppkasper@gmail.com>
2023-03-07 14:45:12 +0100

    Added difficulty scaler + DLL references

---
+++ Difficulty Scaler/Difficulty Scaler (VS C# Project)/Main.cs
@@ -0,0 +1,130 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Jotunn.Managers;
+using Jotunn.Utils;
+using ServerSync;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cozyheim.DifficultyScaler
+{
+    [BepInPlugin(GUID, modName, version)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class Main : BaseUnityPlugin
+    {
+        // Mod information
+        internal const string modName = "DifficultyScaler";
+        internal const string version = "0.0.1";
+        internal const string GUID = "dk.thrakal." + modName;
+
+        // Core objects that is required to patch and configure the mod
+        private readonly Harmony harmony = new Harmony(GUID);
+        internal static ConfigSync configSync = new ConfigSync(GUID) { DisplayName = modName, CurrentVersion = version, MinimumRequiredVersion = version };
+        internal static ConfigFile configFile;
+
+        // Config entries
+        internal static ConfigEntry<bool> modEnabled;
+        internal static ConfigEntry<bool> debugEnabled;
+
+        internal static ConfigEntry<float> overallHealthMultipler;
+        internal static ConfigEntry<float> overallDamageMultipler;
+
+        internal static ConfigEntry<string> monsterHealthModifier;
+        internal static ConfigEntry<string> monsterDamageModifier;
+
+        internal static Dictionary<string, float> monsterHealth;
+        internal static Dictionary<string, float> monsterDamage;
+
+        void Awake()
+        {
+            harmony.PatchAll();
+            configFile = new ConfigFile(BepInEx.Paths.ConfigPath + "/Cozyheim/" + modName + "_Config.cfg", true);
+            configFile.SaveOnConfigSet = true;
+
+            // Assigning config entries
+            modEnabled = CreateConfigEntry("00_General", "ModEnabled", true, "Enable this mod", false);
+            debugEnabled = CreateConfigEntry("00_General", "DebugEnabled", true, "Display debug messages in the console", true);
+
+            overallHealthMultipler = CreateConfigEntry("01_Monsters", "overallHealthMultipler", 1f, "Increases the base health of all monsters in the game. (1 = No change, 1.5 = 50% more health). This stack with the individual monster modifiers.", true);
+            overallDamageMultipler = CreateConfigEntry("01_Monsters", "overallDamageMultipler", 1f, "Increases the base damage of all monsters in the game. (1 = No change, 1.5 = 50% more damage). This stack with the individual monster modifiers.", true);
+            monsterHealthModifier = CreateConfigEntry("01_Monsters", "monsterBaseHealthModifier", "Skeleton:60,Greyling:25", "Set the base health (60 = 60 base health) of individual monsters. Format must follow: Monstername:Health (example: Skeleton:60,Greyling:25)", true);
+            monsterDamageModifier = CreateConfigEntry("01_Monsters", "monsterDamageModifier", "Skeleton:110,Greyling:120", "Set a damage multiplier (110 = +10% increased damage) of individual monsters. Format must follow: Monstername:Damage (example: Skeleton:110,Greyling:120)", true);
+
+            CommandManager.Instance.AddConsoleCommand(new ConsoleLog());
+
+            PrefabManager.OnVanillaPrefabsAvailable += CreateMonsterHealthDictionary;
+        }
+
+        public static void CreateMonsterHealthDictionary()
+        {
+            monsterHealth = new Dictionary<string, float>();
+            monsterHealth.Clear();
+            ConsoleLog.Print("-- Changing base HEALTH for monsters: --", LogType.Message);
+
+            string[] monsterArray = monsterHealthModifier.Value.Split(',');
+            for (int i = 0; i < monsterArray.Length; i++)
+            {
+                string[] monsterData = monsterArray[i].Split(':');
+                if (monsterData.Length >= 2)
+                {
+                    if (float.TryParse(monsterData[1], out float health))
+                    {
+                        float originalHealth = PrefabManager.Instance.GetPrefab(monsterData[0]).GetComponent<Humanoid>().m_health;
+                        monsterHealth.Add(monsterData[0] + "(Clone)", health);
+                        ConsoleLog.Print(monsterData[0] + ": (" + originalHealth + " -> " + health + " HP)");
+                    }
+                }
+            }
+
+            CreateMonsterDamageDictionary();
+            PrefabManager.OnVanillaPrefabsAvailable -= CreateMonsterHealthDictionary;
+        }
+
+        public static void CreateMonsterDamageDictionary()
+        {
+            monsterDamage = new Dictionary<string, float>();
+            monsterDamage.Clear();
+            ConsoleLog.Print("-- Changing base DAMAGE for monsters: --", LogType.Message);
+
+            string[] monsterArray = monsterDamageModifier.Value.Split(',');
+            for (int i = 0; i < monsterArray.Length; i++)
+            {
+                string[] monsterData = monsterArray[i].Split(':');
+                if (monsterData.Length >= 2)
+                {
+                    if (float.TryParse(monsterData[1], out float damage))
+                    {
+                        ConsoleLog.Print(monsterData[0] + ": " + damage + "% damage");
+                        damage /= 100f;
+                        monsterDamage.Add(monsterData[0] + "(Clone)", damage);
+                    }
+                }
+            }
+
+            ConsoleLog.Print("-- Adjusting ALL monster's base damage and health: --", LogType.Message);
+            ConsoleLog.Print("Base health set to " + (overallHealthMultipler.Value * 100).ToString("N0") + "%");
+            ConsoleLog.Print("Base damage set to " + (overallDamageMultipler.Value * 100).ToString("N0") + "%");
+        }
+
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        #region CreateConfigEntry Wrapper
+        ConfigEntry<T> CreateConfigEntry<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = configFile.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> CreateConfigEntry<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => CreateConfigEntry(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        #endregion
+    }
+}
---
+++ Difficulty Scaler/Difficulty Scaler (VS C# Project)/MonsterModifier.cs
@@ -0,0 +1,71 @@
+﻿using HarmonyLib;
+using System.Threading;
+
+namespace Cozyheim.DifficultyScaler
+{
+    internal class MonsterModifier
+    {
+        [HarmonyPatch]
+        private class Patch
+        {
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(Character), "SetupMaxHealth")]
+            private static void Character_SetupMaxHealth_Prefix(Character __instance, ref float ___m_health)
+            {
+                if (__instance != null)
+                {
+                    if (__instance.m_faction != Character.Faction.Players)
+                    {
+                        if (Main.monsterHealth.TryGetValue(__instance.name, out float value))
+                        {
+                            ___m_health = value;
+                        }
+
+                        ___m_health *= Main.overallHealthMultipler.Value;
+                    }
+                }
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(Character), "ApplyDamage")]
+            private static void Character_ApplyDamage_Prefix(Character __instance, ref HitData hit)
+            {
+                if (hit.HaveAttacker())
+                {
+                    if (__instance.m_faction == Character.Faction.Players)
+                    {
+                        float multiplier = 1f;
+
+                        if (Main.monsterDamage.TryGetValue(hit.GetAttacker().name, out float value))
+                        {
+                            multiplier *= value;
+                        }
+                        multiplier *= Main.overallDamageMultipler.Value;
+
+                        hit.ApplyModifier(multiplier);
+                    }
+                }
+            }
+
+            [HarmonyPrefix]
+            [HarmonyPatch(typeof(WearNTear), "RPC_Damage")]
+            private static void WearNTear_RPC_Damage_Prefix(ref HitData hit)
+            {
+                if(hit != null)
+                {
+                    if (hit.GetAttacker().m_faction != Character.Faction.Players)
+                    {
+                        float multiplier = 1f;
+                        if (Main.monsterHealth.TryGetValue(hit.GetAttacker().name, out float value))
+                        {
+                            multiplier *= value;
+                        }
+                        multiplier *= Main.overallDamageMultipler.Value;
+
+                        hit.ApplyModifier(1f / multiplier);
+                    }
+                }
+            }
+        }
+    }
+}
