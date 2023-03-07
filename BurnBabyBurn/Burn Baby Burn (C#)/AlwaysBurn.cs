a046a5146b7c8a7ccd3030272a466c7937f9f6d4
Kasper Topp <toppkasper@gmail.com>
2023-03-07 14:49:24 +0100

    Added BurnBabyBurn

---
+++ BurnBabyBurn/Burn Baby Burn (C#)/AlwaysBurn.cs
@@ -0,0 +1,99 @@
+﻿using HarmonyLib;
+using Jotunn.Managers;
+using UnityEngine;
+using System;
+
+namespace Cozyheim.BurnBabyBurn
+{
+    [HarmonyPatch]
+    internal class AlwaysBurn
+    {
+        // ref bool __result = returned value of the original method
+        // ref <T> __instance = reference to original class (this)
+        // object[] __args = original parameters
+        // -> Example: Player player = (Player) __args[0];
+        
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Fireplace), "Awake")]
+        private static void Fireplace_Awake_Postfix(ref float ___m_startFuel, ref float ___m_maxFuel, Fireplace __instance)
+        {
+            ConsoleLog.Print("Awake fireplace!");
+            if (IsExcluded(__instance)) return;
+
+            ConsoleLog.Print("Not excluded: " + __instance.name);
+            ___m_maxFuel = ___m_startFuel;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Fireplace), "UpdateFireplace")]
+        private static void Fireplace_UpdateFireplace_Postfix(Fireplace __instance)
+        {
+            if (IsExcluded(__instance)) return;
+
+            __instance.CancelInvoke("UpdateFireplace");
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Fireplace), "CheckEnv")]
+        private static void Fireplace_CheckEnv_Postfix(Fireplace __instance)
+        {
+            if (IsExcluded(__instance)) return;
+
+            __instance.CancelInvoke("CheckEnv");
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Fireplace), "IsBurning")]
+        private static bool Fireplace_IsBurning_Prefix(ref bool __result, Fireplace __instance)
+        {
+            if (IsExcluded(__instance))
+                return true;
+
+            __result = true;
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Fireplace), "Interact")]
+        private static bool Fireplace_Interact_Prefix(ref bool __result, Fireplace __instance)
+        {
+            if (IsExcluded(__instance))
+                return true;
+
+            __result = false;
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Fireplace), "GetHoverText")]
+        private static bool Fireplace_GetHoverText_Prefix(ref string __result, string ___m_name, Fireplace __instance)
+        {
+            if (IsExcluded(__instance))
+                return true;
+
+            string text = Localization.instance.Localize(___m_name) + "\n";
+            text += "<color=yellow>No fuel needed</color>";
+            __result = text;
+            return false;
+        }
+
+        private static bool IsExcluded(Fireplace fireplace)
+        {
+            if (Main.excludedFireplacesList.Count > 0)
+            {
+                foreach (string s in Main.excludedFireplacesList)
+                {
+                    if(s != "")
+                    {
+                        if (fireplace.name.StartsWith(s))
+                            return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        
+    }
+    
+}
---
+++ BurnBabyBurn/Burn Baby Burn (C#)/Main.cs
@@ -0,0 +1,80 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using Jotunn.Managers;
+using Jotunn.Utils;
+using ServerSync;
+using System.Collections.Generic;
+
+namespace Cozyheim.BurnBabyBurn
+{
+    [BepInPlugin(GUID, modName, version)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
+    internal class Main : BaseUnityPlugin
+    {
+        // Mod information
+        internal const string modName = "BurnBabyBurn";
+        internal const string version = "0.0.1";
+        internal const string GUID = "dk.thrakal." + modName;
+
+        // Core objects that is required to patch and configure the mod
+        private readonly Harmony harmony = new Harmony(GUID);
+        internal static ConfigSync configSync = new ConfigSync(GUID) { DisplayName = modName, CurrentVersion = version, MinimumRequiredVersion = version };
+        internal static ConfigFile configFile;
+
+        // Config entries
+        internal static ConfigEntry<bool> modEnabled;
+        internal static ConfigEntry<bool> debugEnabled;
+
+        internal static ConfigEntry<string> excludedFireplaces;
+
+        internal static List<string> excludedFireplacesList = new List<string>();
+
+        void Awake()
+        {
+            configFile = new ConfigFile(BepInEx.Paths.ConfigPath + "/Cozyheim/" + modName + "_Config.cfg", true);
+            configFile.SaveOnConfigSet = true;
+
+            // Assigning config entries
+            modEnabled = CreateConfigEntry("General", "ModEnabled", true, "Enable this mod", false); // false = non-synced (client)
+            debugEnabled = CreateConfigEntry("General", "DebugEnabled", false, "Display debug messages in the console", true); // true = synced (server)
+            excludedFireplaces = CreateConfigEntry("General", "ExcludedFireplaces", "", "Add name of fireplaces which must be excluded. Seperate by comma", true); // true = synced 
+
+            // Removes this object if the mod is not enabled
+            if (!modEnabled.Value)
+            {
+                Destroy(gameObject);
+            }
+
+            string[] fireplaces = excludedFireplaces.Value.Split(',');
+            excludedFireplacesList = new List<string>();
+            foreach (string s in fireplaces)
+            {
+                excludedFireplacesList.Add(s.Replace(" ", ""));
+            }
+
+            harmony.PatchAll();
+            CommandManager.Instance.AddConsoleCommand(new ConsoleLog());
+        }
+
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+
+        #region CreateConfigEntry Wrapper
+        ConfigEntry<T> CreateConfigEntry<T>(string group, string name, T value, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = configFile.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> CreateConfigEntry<T>(string group, string name, T value, string description, bool synchronizedSetting = true) => CreateConfigEntry(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        #endregion
+    }
+}
